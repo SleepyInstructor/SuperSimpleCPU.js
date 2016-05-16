e4235e0531c99ff673b15b32540d7d47caba462d
Kim Lam <lamkim@hotmail.com>
2016-05-15 22:00:00 -0700

    First Commit

---
+++ simplecomputer.js
@@ -0,0 +1,284 @@
+/*jshint
+   moz : true
+*/
+/*globals
+   class
+*/
+//Class basic computer 
+function leftPad(size, baseString, padChar) {
+    "use strict";
+    return Array(size - baseString.length).fill(padChar).join("") + baseString;
+}
+
+function twosComplementToNumber(encodedString) {
+    "use strict";
+    var leadingOne = encodedString[0] === '1';
+    var newString = leadingOne ?
+        encodedString.split("").map(function(val) {return val === '0' ? '1' : '0'; }).join("") :
+        encodedString;
+    return leadingOne ? -(parseInt(newString, 2) + 1) : parseInt(newString, 2);
+}
+
+function numberToTwosComplement(number, numBits) {
+    "use strict";
+    var neg = number < 0;
+    var posNum = !neg ? Math.abs(number) : Math.abs(number) - 1;
+    var baseNumber = leftPad(numBits, posNum.toString(2), '0');
+    var newNumber = neg ?
+        baseNumber.split("").map(function(val) {return val === '0' ? '1' : '0'; }).join("") :
+        baseNumber;
+    return newNumber;
+}
+function binaryToNumber(encodedString){
+    return parseInt(encodedString,2);
+}
+function numberToBinary(number,numbits){
+    return leftpad(numbits, nummber.toString(2),"0");
+}
+//The CPU with the the registers and opcodes
+class CPU {
+    constructor(inputCallback, outputCallback) {
+        this.inputCallback = inputCallback;
+        this.outputCallback = outputCallback;
+        this.CPUstates = {
+            stopped: "stopped",
+            normal: "normal"
+        };
+        this.state = this.CPUstates.normal;
+        this.registers = {
+            pc: 0,
+            acc: 0,
+            temp: 0,
+            ir: 0
+        };
+
+        this.instructionSet = {
+            "STP": {
+                opcode: "1111",
+                execute: function(operand) {
+                    this.state = this.CPUstates.stopped;
+                }.bind(this)
+            },
+            "ADD": {
+                opcode: "0001",
+                execute: function(operand, memory) {
+                    this.registers.acc += twosComplementToNumber(operand);
+                }.bind(this),
+                decodeOperand : twosComplementToNumber,
+                encodeOperand : numberToTwosComplement
+            },
+            "SUB": {
+                opcode: "0010",
+                execute: function(operand, memory) {
+                    this.registers.acc -= twosComplementToNumber(operand);
+                }.bind(this),
+                decodeOperand : twosComplementToNumber,
+                encodeOperand : numberToTwosComplement
+            },
+            "LOD": {
+                opcode: "0011",
+                load: function(operand, memory) {
+                    var address = parseInt(operand, 2);
+                    var value = twosComplementToNumber(memory[address]);
+                    this.registers.acc = value;
+                }.bind(this),
+                decodeOperand : binaryToNumber,
+                encodeOperand : numberToBinary
+            },
+            "LDI": {
+                opcode: "0100",
+                load: function(operand, memory) {
+                    this.registers.acc = twosComplementToNumber(operand);
+                }.bind(this),
+                decodeOperand : twosComplementToNumber,
+                encodeOperand : numberToTwosComplement
+            },
+            "STO": {
+                opcode: "0101",
+                execute: function(operand, memory) {
+                    var address = parseInt(operand, 2);
+                    memory[address] = numberToTwosComplement(this.registers.acc, 16);
+                }.bind(this),
+                decodeOperand : binaryToNumber,
+                encodeOperand : numberToBinary
+            },
+            "INP": {
+                opcode: "0110",
+                execute: function(operand) {
+                    this.registers.acc = this.inputCallback();
+                }.bind(this),
+                
+            },
+            "OUT": {
+                opcode: "0111",
+                execute: function(operand) {
+                    this.outputCallback(this.registers.acc);
+                }.bind(this)
+            },
+            "JMP": {
+                opcode: "1000",
+                execute: function(operand) {
+                    this.registers.pc = parseInt(operand, 2);
+                }.bind(this),
+                decodeOperand : binaryToNumber,
+                encodeOperand : numberToBinary
+            },
+            "JNG": {
+                opcode: "1001",
+                execute: function(operand) {
+                    if (this.registers.acc < 0) {
+                        this.registers.pc = parseInt(operand, 2);
+                    }
+                }.bind(this),
+                decodeOperand : binaryToNumber,
+                encodeOperand : numberToBinary
+            },
+            "JZR": {
+                opcode: "1010",
+                execute: function(operand) {
+                    if (this.registers.acc === 0) {
+                        this.registers.pc = parseInt(operand, 2);
+                    }
+                }.bind(this),
+                decodeOperand : binaryToNumber,
+                encodeOperand : numberToBinary
+            }
+
+        };
+        this.forwardLookupTable = function(IS) {
+            var table = {};
+            for (var prop in IS) {
+                table[prop] = IS[prop].opcode;
+            }
+            return table;
+        }(this.instructionSet);
+        this.reverseLookupTable = function(IS) {
+            var table = {};
+            for (var prop in IS) {
+                table[IS[prop].opcode] = prop;
+            }
+            return table;
+        }(this.instructionSet);
+    }
+    reset() {
+        for (var prop in this.registers) {
+            this.registers[prop] = 0;
+        }
+        this.state = this.CPUstates.normal;
+    }
+
+}
+
+//Basic computer with 4 micro-operations
+//Fetch phase: get the instruction from memory
+//             Update the PC
+//Decode:      seperate the operand and operator
+//load:        load and data from memory to the appropriate register
+//exec:        Run the instruction, which can include one of
+//             1)Modifying the registers
+//             2)storing data to memory
+//             3)Changing the PC based on jump statements
+class computer {
+    constructor(config) {
+        if (typeof(config) !== "object") {
+            console.error("Expected configuration object as input parameter.");
+            console.error("{ memorySize: ..., inputCallback : function(){...}, outputCallback : function(outWord){...}}");
+            return;
+        }
+        this.cpu = new CPU(config.inputCallback || function() { return 0;}, 
+                           config.outputCallback || function(outWord) {});
+        this.memory = new Array(config.memorySize || 16); //default 16 words
+        this.opcode = "";
+        this.operand = "";
+
+        this.ProcessorStates = {
+            fetch: "fetch",
+            decode: "decode",
+            load: "load",
+            exec: "exec"
+        };
+        this.NextState = {
+            fetch: "decode",
+            decode: "load",
+            load: "exec",
+            exec: "fetch"
+        };
+        this.currentState = this.ProcessorStates.fetch;
+    }
+    //CPU actions based on state
+    fetch() {
+        //fetch retries the instruction from memory
+        this.cpu.registers.ir = this.memory[this.cpu.registers.pc];
+        this.cpu.registers.pc += 1;
+    }
+    decode() {
+
+        var i = 0;
+        var irString = this.cpu.registers.ir;
+        var found = false;
+        for (i = 1; i < irString.length; i += 1) {
+            if (this.cpu.reverseLookupTable.hasOwnProperty(irString.substring(0, i))) {
+                this.opcode = this.cpu.reverseLookupTable[irString.substring(0, i)];
+                this.operand = irString.substring(i, irString.length);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            this.cpu.state = this.cpu.CPUstates.stopped;
+
+        } else if (this.cpu.instructionSet[this.opcode].hasOwnProperty("loadSetup")) {
+            this.cpu.instructionSet[this.opcode].loadSetup(this.operand);
+        }
+
+    }
+    load() {
+        if (this.cpu.instructionSet[this.opcode].hasOwnProperty("load")) {
+            this.cpu.instructionSet[this.opcode].load(this.operand);
+        }
+    }
+    exec() {
+        if (this.cpu.instructionSet[this.opcode].hasOwnProperty("execute")) {
+            this.cpu.instructionSet[this.opcode].execute(this.operand);
+        }
+
+    }
+    //Changes the content of the memory
+    uploadMemory(mem) {
+        this.memory = mem.slice();
+    }
+    //return a copy of the memory
+    dumpMemory() {
+        return this.memory.slice();
+    }
+
+    //Reset the CPU, the cpu will not run if it
+    //enters a stopped state, and needs to be reset.
+    reset() {
+        this.cpu.reset();
+        this.currentState = this.ProcessorStates.fetch;
+    }
+    //If an instruction is in a the middle of the decode/fetch/load/execute cycle
+    //this command will finish that cycle.
+    //If the cpu is in the fectch state, it will fininsh one complete cycle
+    executeNextInstruction() {
+        if (this.cpu.state === this.cpu.CPUstates.normal) {
+            switch (this.currentState) {
+                case this.ProcessorStates.fetch:
+                    this.executeClockPulse();
+                case this.ProcessorStates.decode:
+                    this.executeClockPulse();
+                case this.ProcessorStates.load:
+                    this.executeClockPulse();
+                case this.ProcessorStates.exec:
+                    this.executeClockPulse();
+            }
+        }
+    }
+    //Execute 1 step of the cycle
+    //This allows illustration of the cycle in the UI.
+    executeClockPulse() {
+        this[this.currentState]();
+        this.currentState = this.NextState[this.currentState];
+    }
+}
---
+++ testprogram.js
@@ -0,0 +1,159 @@
+
+//Setup simulated CPU
+var webcomputer = new computer({
+    memorySize : 16,
+    inputCallback : function(){
+        "use strict";
+        var element = document.getElementById("input");
+        return element.value;
+    },
+    outputCallback: function(output){
+        "use strict";
+        var element = document.getElementById("output");
+        element.innerHTML = output;
+    }
+});
+webcomputer.uploadMemory(["0001000000000001",
+                          "0001000000000010",
+                          "1111000000000000", 
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                          "1111000000000000",
+                         "1111000000000000"]);
+//Data binding
+//Should probbaly use Angular.js to do this, but I want
+//to avoid dependencies on other projects, so code-behind it is!
+var registersBindings = {
+     registers: webcomputer.cpu.registers,
+     ir : "ir",
+     pc : "pc",
+     acc : "acc",
+    state : "state",
+    
+     update : function(){
+         document.getElementById(this.ir).innerHTML = this.registers.ir;
+         document.getElementById(this.pc).innerHTML = this.registers.pc;
+         document.getElementById(this.acc).innerHTML = this.registers.acc;
+         
+     }
+}
+var memoryBindings = {
+
+    memLabel : "memory",
+    update:function(){
+        document.getElementById(this.memLabel).innerHTML = webcomputer.memory.join("<br>");
+    }
+}
+bindings = [registersBindings, memoryBindings];
+
+//Setup initial display
+bindings.forEach(function(Object,number){
+    Object.update();
+});
+
+//Event HAndlers
+
+function dissassemble(){
+    //aliasing
+    var memory = webcomputer.dumpMemory();
+    var instructionset = webcomputer.cpu.instructionSet;
+    var reverseLookup = webcomputer.cpu.reverseLookupTable;
+    
+    var asm = memory.map(function (bitString, Number){
+        var opcode = bitString.substring(0,4); //Assume 4 bit
+        var operandString = bitString.substring(4);
+        if( reverseLookup.hasOwnProperty(opcode)){
+            pnem = reverseLookup[opcode];
+            operand = (instructionset[pnem].decodeOperand || function(){ return "";})(operandString);
+        }else {
+            pnem = "dat";
+            operand =  twosComplementToNumber(bitString);
+        }
+        
+        return pnem + " " + operand;   
+    });
+    var textString = asm.join("\n");
+    document.getElementById("codeWindow").value = textString;
+     
+}
+function assemble(){
+    //aliases
+    var instructionset = webcomputer.cpu.instructionSet;
+    var lookupTable = webcomputer.cpu.forwardLookupTable;
+    
+    var codeBlock = document.getElementById("codeWindow").value;
+    var errorLog = [];
+    var errorCount = 0;
+    var temp= codeBlock.trim().split("\n");
+    var asm = temp.map(function(string, lineNum){
+        var codeArray = string.trim().split(" ");
+        var opcode = codeArray[0];
+        if(opcode === "dat"){
+            //data
+            if(codeArray.length < 2){
+                errorCount += 1;
+                errorLog.push("Line " + lineNum+": No operand for dat");
+                return "0000000000000000";
+            } else {
+                return numberToTwosComplement(codeArray[1],16);
+            }
+        } else if(instructionset.hasOwnProperty(codeArray[0])){
+            var opcode = instructionset[codeArray[0]].opcode;
+            var operand= "000000000000";
+            if(instructionset[codeArray[0]].hasOwnProperty("encodeOperand")){
+                if(!isNaN(codeArray[1])){
+                    operand = instructionset[codeArray[0]].encodeOperand(Number(codeArray[1]),12);
+                } else {
+                    errorLog.push("Line " + lineNum+": invalid operand");
+                    errorCount +=1;
+                }
+            } else {
+                if(codeArray.length > 1){
+                  errorLog.push("Line " + lineNum+": Unexpected operand");
+                    errorCount +=1;
+                }
+            }
+           return opcode + operand;
+                  
+        } else {
+                  errorCount +=1;
+                  errorLog.push("Line " + lineNum+": opcode not recognized");
+                 return "0000000000000000";
+        }    
+    });
+    if(errorCount === 0){
+        if(asm.length < 16){
+            for(var i = asm.length; i < 16; i += 1){
+                asm.push("0000000000000000");
+            }
+        }
+        webcomputer.uploadMemory(asm);
+    } else {
+        document.getElementById("log").innerHTML = errorLog.join("<br>");
+    }
+    bindings.forEach(function(Object,number){
+    Object.update();});
+}
+
+function nextInstruction(){
+    webcomputer.executeNextInstruction();
+    bindings.forEach(function(Object,number){
+    Object.update();
+});
+}
+function resetComputer(){
+    webcomputer.reset();
+    bindings.forEach(function(Object,number){
+    Object.update();
+});
+}
+                     
